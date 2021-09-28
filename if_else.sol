dbace3cd34669d69aedc8fb60bd05f4208f8260d
Никита Кошманов <31765932+NikitaKoshmanov@users.noreply.github.com>
2021-09-28 05:25:56 +0300

    Create if_else.sol

---
+++ if_else.sol
@@ -0,0 +1,33 @@
+pragma solidity 0.8.7;
+
+contract helloWorld{
+    struct Company{
+        string name;
+        uint age;
+        uint workers;
+        string status;
+    }
+    
+    mapping(address => Company) private companies;
+    
+    function addCompany(string memory name, uint age, uint workers) public{
+        address sender = msg.sender;
+        Company memory newCompany;
+        newCompany.name = name;
+        newCompany.age = age;
+        newCompany.workers = workers;
+        if(workers <= 10){
+            newCompany.status = 'small';
+        }else if(workers > 10 && workers <= 30){
+            newCompany.status = 'medium';
+        }else{
+            newCompany.status = 'huge';
+        }
+            companies[sender] = newCompany;
+    }
+    
+    function getCompany() public view returns(string memory name, uint age, uint workers, string memory status){
+        addres sender = msg.sender;
+        return(companies[sender].name, companies[sender].age, companies[sender].workers, companies[sender].status)
+    }
+}
